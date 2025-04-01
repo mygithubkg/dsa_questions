037a1dcba28bd9918f175f03e26d40d7f5fbfaa6
Karrtik Gupta <karrtikschoolid11@gmail.com>
2025-04-01 22:48:56 +0530

    Tree questions type

---
+++ Binary Tree/BFS/View of Tree/leftview.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+
+using namespace std;
+
+struct Node
+{
+int data;
+struct Node* left;
+struct Node* right;
+
+Node(int x){
+    data = x;
+    left = right = NULL;
+}
+};
+
+
+class Solution {
+public:
+    vector<int> leftView(Node *root) {
+        vector<int> ans; 
+        if (root == NULL) return ans;
+        queue<Node*> q;
+        q.push(root);
+        
+        while (!q.empty()){
+            int n = q.size();
+            
+            for (int i =0; i < n; i++){
+                Node* node = q.front();
+                q.pop();
+                if (node->left != NULL) q.push(node->left);
+                if (node->right != NULL) q.push(node->right);
+                if (i==0){
+                    ans.push_back(node -> data);
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+int main(){
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+
+    Solution obj;
+    vector<int> ans = obj.leftView(root);
+    for (auto i: ans){
+        cout<<i<<" ";
+    }
+    return 0;   
+}
